d4340209ed7d6acf30ea9a3a5bd03384acf22192
daniel <farzidaniel16@gmail.com>
2023-09-05 01:11:51 +0300

    changing protocol

--- src/api/cityApi.js
+++ src/api/cityApi.js
@@ -1,11 +1,7 @@
-
-import axios from 'axios'
-axios.defaults.withCredentials = false
+import axios from "axios";
+axios.defaults.withCredentials = false;
 
 export default axios.create({
-    baseURL: "http://api.openweathermap.org/geo/1.0/direct?limit=5&appid=96c4a9454b5950ee36e59454486c034c",
-
-})
-
-
-
+  baseURL:
+    "https://api.openweathermap.org/geo/1.0/direct?limit=5&appid=96c4a9454b5950ee36e59454486c034c",
+});
--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,11 +1,7 @@
-
-import axios from 'axios'
-axios.defaults.withCredentials = false
+import axios from "axios";
+axios.defaults.withCredentials = false;
 
 export default axios.create({
-    baseURL: "https://api.openweathermap.org/data/2.5/weather?appid=96c4a9454b5950ee36e59454486c034c",
-
-})
-
-
-
+  baseURL:
+    "https://api.openweathermap.org/data/2.5/weather?appid=96c4a9454b5950ee36e59454486c034c",
+});
